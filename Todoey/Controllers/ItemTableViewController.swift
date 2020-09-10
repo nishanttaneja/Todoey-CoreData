6500bac8ffee97ef098c41cef05346e2c61200f1
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-10 13:35:09 +0530

    item checkmark enabled

--- Todoey/Controllers/ItemTableViewController.swift
+++ Todoey/Controllers/ItemTableViewController.swift
@@ -46,12 +46,19 @@ class ItemTableViewController: UITableViewController {
     }
 
     //MARK:- TableView Delegate|DataSource
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        items[indexPath.row].done = !items[indexPath.row].done
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row].title
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
