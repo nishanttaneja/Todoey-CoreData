9b81f2f8831727bac70858e6e27454f20098108a
nishanttaneja <nishant.taneja2701@gmail.com>
2020-09-10 11:33:18 +0530

    implement loading data

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -7,10 +7,45 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UIViewController {
     // IBOutlet
     @IBOutlet weak var tableView: UITableView!
+    
+    // Initialise
+    private var categories = [Category]()
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // Override View Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
     // IBAction
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {}
+    
+    //MARK:- CoreData
+    /// This function is used to fetch categories.
+    private func loadCategories() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try context.fetch(request);
+            tableView.reloadData()
+        }
+        catch {print("error loading categories")}
+    }
+}
+
+//MARK:- TableView Delegate|DataSource
+extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
 }
